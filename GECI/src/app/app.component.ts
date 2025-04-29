462de0e5c57bdb1c35bc1272ee5558c8f1bf498d
Hurtado Escobar <keiner-matias.hurtado@getronics.com>
2025-04-29 13:40:33 +0200

    reemplazar las imagenes por las reales

--- GECI/src/app/app.component.ts
+++ GECI/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderWithoutLoginComponent } from "./components/header-without-login/header-without-login.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderWithoutLoginComponent],
+  imports: [RouterOutlet, HeaderWithoutLoginComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
