d3f2f262d037bad9924af491028101e13bdf5cb7
Hurtado Escobar <keiner-matias.hurtado@getronics.com>
2025-04-30 12:11:38 +0200

    lgon

--- GECI/src/app/app.routes.ts
+++ GECI/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { InicioComponent } from './pages/inicio/inicio.component';
+export const routes: Routes = [
+  {
+    path: '',
+    component: InicioComponent
+  }
+];
