e5b495b673efe7598243a56193d6899a20f02e5d
Lorenzob05 <98970203+Lorenzob05@users.noreply.github.com>
2025-04-30 12:07:14 +0200

    header logueado

--- GECI/src/app/components/header/header.component.ts
+++ GECI/src/app/components/header/header.component.ts
@@ -1,14 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatCardModule, MatIconModule],
+  imports: [MatCardModule, MatIconModule, MatDividerModule,MatButtonModule, MatMenuModule, NgIf],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  menuVisible = false;
 
+  toggleMenu(): void {
+    this.menuVisible = !this.menuVisible;
+  }
+
+  @HostListener('document:click', ['$event'])
+  handleClickOutside(event: MouseEvent): void {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.profile')) {
+      this.menuVisible = false;
+    }
+  }
+
+  onOrganismos(): void {
+    console.log('Organismos clicked');
+    this.menuVisible = false;
+  }
+
+  onDatosUsuario(): void {
+    console.log('Datos de usuario clicked');
+    this.menuVisible = false;
+  }
+
+  onSalir(): void {
+    console.log('Salir clicked');
+    this.menuVisible = false;
+  }
 }
